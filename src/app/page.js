85959c970a26d5131a5c408b0d771a25f0cb3e8f
test-bot-69 <testbot.1831@gmail.com>
2023-08-17 19:33:21 +0530

    Created home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,34 @@
 import Image from 'next/image'
+import localFont from 'next/font/local'
+
+const batmanForeverA = localFont({ src: '../fonts/batman_forever/batmfa__.ttf' })
+const batmanForeverO = localFont({ src: '../fonts/batman_forever/batmfo__.ttf' })
+const automata = localFont({ src: '../fonts/automata/AUTOMATA-DISPLAY.ttf' })
 
 export default function Home() {
   return (
-    <home>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur minus, hic deserunt iure sint exercitationem asperiores, mollitia quas vitae laborum quo labore assumenda vero expedita enim iusto architecto ipsa eius.
-    </home>
+    <div className='home text-white flex'>
+      <div className='w-1/2 text-sky-300'>
+        {/* <Image src={"/profilePic.png"} width={400} height={400} alt='Omkar Ghodake' className='rounded-full fixed top-[24vh] left-[15.5vw]' /> */}
+      </div>
+
+      <div className='w-1/2 h-full flex flex-col pt-[15vh] px-10 space-y-10'>
+        <div className="flex flex-col space-y-5">
+          <h1 className={`text-right text-8xl text-white border-b w-fit ml-auto ${batmanForeverO.className}`}>WELCOME</h1>
+
+          <h2 className={`text-right text-6xl border-b border-sky-400 py-2`}>
+            <span className={`${batmanForeverA.className} text-white`}>I'M </span>
+            <span className={`${batmanForeverO.className} text-sky-300`}>OMKAR GHODAKE</span>
+          </h2>
+
+        </div>
+
+        <h3 className={`${batmanForeverA.className} text-4xl text-sky-400`}>BLOCKCHAIN APPLICATION DEVELOPER</h3>
+
+        <p className={`text-xl ${automata.className} tracking-wider text-justify leading-8 text-white`}>
+          A Proficient <span className='text-sky-300'>Blockchain Developer</span> interested in contributing expertise in <span className='text-sky-300'>Web</span> and <span className='text-sky-300'>Blockchain App Development.</span> Passionate <span className='text-sky-300'>BE Student</span> with a good hands-on experience in <span className='text-sky-300'>Full Stack Web Development</span> with various web frameworks and libraries. Deeply knowledgeable in <span className='text-sky-300'>Cryptocurrency, Decentralized Applications (DApps), Smart Contracts, Distributed Ledger</span> along with slight knowledge of <span className='text-sky-300'>Blockchain Mining</span> and <span className='text-sky-300'>Cryptography.</span>
+        </p>
+      </div>
+    </div>
   )
 }
---
+++ src/app/temp/page.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Temp = () => {
+  return (
+    <div>
+      <p className='bg-sky-400/0 text-sky-400 w-[26vw] absolute top-[2vh] left-[38vw] p-5 border border-sky-400'>
+        0 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate repellat error provident alias velit nam molestiae reiciendis eius excepturi omnis distinctio, officia quasi eos non debitis fuga, adipisci repudiandae autem.
+      </p>
+      <p className='bg-sky-400/5 text-sky-400 w-[26vw] absolute top-[28vh] left-[38vw] p-5 border border-sky-400'>
+        5 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate repellat error provident alias velit nam molestiae reiciendis eius excepturi omnis distinctio, officia quasi eos non debitis fuga, adipisci repudiandae autem.
+      </p>
+      <p className='bg-sky-400/10 text-sky-400 w-[26vw] absolute top-[53vh] left-[38vw] p-5 border border-sky-400'>
+        10 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate repellat error provident alias velit nam molestiae reiciendis eius excepturi omnis distinctio, officia quasi eos non debitis fuga, adipisci repudiandae autem.
+      </p>
+      <p className='bg-sky-400/20 text-sky-400 w-[26vw] absolute top-[78vh] left-[38vw] p-5 border border-sky-400'>
+        20 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate repellat error provident alias velit nam molestiae reiciendis eius excepturi omnis distinctio, officia quasi eos non debitis fuga, adipisci repudiandae autem.
+      </p>
+    </div>
+  )
+}
+
+export default Temp
